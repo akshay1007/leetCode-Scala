953417923aa5ce3919d913f5ede0ea6c88cd128c
akshay jain <akshay.jain@mfine.co>
2021-03-24 13:48:34 +0530

    added LongestSubString program

--- scaladsa/src/main/scala/dsa/LongestSubString.scala
+++ scaladsa/src/main/scala/dsa/LongestSubString.scala
@@ -1,14 +1,21 @@
 package dsa
 import scala.collection.mutable
+/*
+ Scala program to find and print longest substring without repeating characters.
+ Function to find and print longest substring without repeating characters.
+*/
+
 object LongestSubString extends App{
 
   def longestSubstring(s:String):Int= {
-
+    //starting point of current substring.
     var start = 0
+    // maximum length substring without  repeating characters.
     var length = 0
 
     if(s.isEmpty) 0
     else {
+      //Hash Map to store last occurrence of each already visited character.
       var lookup = mutable.HashMap[Char,Int]()
 
       for((c,i) <- s.zipWithIndex) {
