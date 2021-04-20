4e6f87af00c9b9cd19783afec6fd01a9048c188c
akshay jain <akshay.jain@mfine.co>
2021-04-20 15:22:56 +0530

    Running Sum of 1d Array

---
+++ scaladsa/src/main/scala/dsa/RunningSumOfArray.scala
@@ -0,0 +1,27 @@
+package dsa
+
+object RunningSumOfArray extends App {
+
+
+  def runningSum(nums: Array[Int]): Array[Int] = {
+            var previous_val = 0
+            var output:Array[Int] = new Array[Int](nums.length)
+            for(i<-0 until(nums.length)){
+           previous_val = previous_val+nums(i)
+              output(i)= previous_val
+            }
+            output
+  }
+
+
+
+  val Input:Array[Int] = Array(3,1,2,10,1)
+  val output = runningSum(Input)
+  output.foreach(x => print(x + " "))
+
+
+}
+
+
+
+
