cc4661b631cf5259108c786e0a907823908336a2
akshay jain <akshay.jain@mfine.co>
2021-04-24 22:31:38 +0530

    Subtract the Product and Sum of Digits of an Integer

--- scaladsa/src/main/scala/dsa/SubtractProductAndSum.scala
+++ scaladsa/src/main/scala/dsa/SubtractProductAndSum.scala
@@ -8,18 +8,13 @@ object SubtractProductAndSum extends  App{
     var sum = 0
     while(n>0){
       product = product * (n%10)
-      println("Product : "+ product)
       sum = sum + (n%10)
-      println(("Sum : "+ sum))
       n= n/10
     }
-
-    println("=======" + (product-sum) + " "+ product +" " + sum)
-
     product-sum
 
   }
-
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 val t = 234
   subtractProductAndSum(t)
 }
