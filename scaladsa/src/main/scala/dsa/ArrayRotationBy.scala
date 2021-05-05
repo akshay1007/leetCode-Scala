37440e67d7af94837d207df52c415f4075834b20
akshay jain <akshay.jain@mfine.co>
2021-05-05 09:43:20 +0530

    Array Rotation by K element

--- scaladsa/src/main/scala/dsa/ArrayRotationBy.scala
+++ scaladsa/src/main/scala/dsa/ArrayRotationBy.scala
@@ -12,6 +12,7 @@ object ArrayRotationBy extends App {
 
 
   }
+
   def rotationBy(arr:Array[Int], k :Int)={
 
     def swap(nums: Array[Int], i: Int, j: Int) = {
@@ -31,12 +32,20 @@ object ArrayRotationBy extends App {
 
   }
 
+  def singleLoop(arr:Array[Int], k :Int): Array[Int]={
+    var ouptput = new Array[Int](arr.length)
+    for(i<-0 until arr.length){
+      ouptput((i+k)%arr.length) = arr(i)
+    }
+    ouptput
+
+  }
   var input = Array(1,2,3,4,5,6,7)
   val k = 3
-//rotationBy(input,k).foreach(x => print(x))
-//  println("============")
+  rotationBy(input,k).foreach(x => print(x))
+  println("============")
+  singleLoop(input,k).foreach(x => print(x))
 
-  rotationBy1Line(input,k).foreach(x => print(x))
 
 
 }
