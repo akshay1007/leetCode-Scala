5abc0c132c9f792b16ac30a5f1eb4eefbf1ca2e4
akshay jain <akshay.jain@mfine.co>
2021-05-09 18:34:28 +0530

    added help for for loop

---
+++ scaladsa/src/main/scala/help/forLoop.scala
@@ -0,0 +1,28 @@
+package help
+
+object forLoop extends   App {
+
+  for (i<- 1 to 9 ; j<- 1 to 3){
+    println("Value of i : " + i + " and value of j :" + j)
+  }
+
+  val lst = List(1,2,3,4,5,5,6,7,8,9,10)
+  for(i<- lst){
+    println("List values are :" + i)
+  }
+
+  //using filter
+  for(i<- lst ; if i <6){
+    println("List values are :" + i)
+  }
+
+  //using expression
+
+  val result = for{ i<- lst } yield {
+    i*i
+  }
+  println("Square of elements are " + result)
+
+}
+
+
