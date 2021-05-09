116b0a53984c76ec0967d3db0cb6877442059df6
akshay jain <akshay.jain@mfine.co>
2021-05-09 18:34:31 +0530

    added help for for loop

--- scaladsa/src/main/scala/help/forLoop.scala
+++ scaladsa/src/main/scala/help/forLoop.scala
@@ -2,10 +2,12 @@ package help
 
 object forLoop extends   App {
 
+  //Nested for loop
   for (i<- 1 to 9 ; j<- 1 to 3){
     println("Value of i : " + i + " and value of j :" + j)
   }
 
+  //Using list
   val lst = List(1,2,3,4,5,5,6,7,8,9,10)
   for(i<- lst){
     println("List values are :" + i)
